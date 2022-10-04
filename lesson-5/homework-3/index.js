16b905cb50528376cc13ff60a51819d5c540c945
VelPaulov <velyakishkin@gmail.com>
2022-10-04 16:19:36 +0300

    fixed homework-3

--- lesson-5/homework-3/index.js
+++ lesson-5/homework-3/index.js
@@ -1,18 +1,19 @@
 const num = prompt('Enter a number', 55);
 
 const colonOdd = (number) => {
-    let arr = number.split('');
-
-    let res = arr.map((item, index, array) => {
-        if (!(item % 2 == 0) &&
-        !((array[index + 1]) % 2 == 0) &&
-        !(index === array.length - 1)) {
-            return `${item}:`;
-        }
-        return item;
-    });
-    return res.join('');
-}
+    return number
+        .split("")
+        .map((item, index, array) => {
+            const condition = [item, array[index+1]].every(
+                (item) => item % 2 !== 0
+            );
+            if (condition && !(index === array.length - 1)) {
+                return `${item}:`;
+            }
+            return item;
+        })
+        .join("");
+};
 
 const result = colonOdd(num);
 console.log(result);
