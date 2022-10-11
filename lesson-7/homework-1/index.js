dcc7cbef9533ffbf2e2adc0d060eb69173561601
VelPaulov <velyakishkin@gmail.com>
2022-10-11 16:31:02 +0300

    minor fixes

--- lesson-7/homework-1/index.js
+++ lesson-7/homework-1/index.js
@@ -4,22 +4,18 @@ function Worker1(name, surname, rate, days) {
     this.surname = surname;
     this.rate = rate;
     this.days = days;
-}
-
-const getSalary = function() {
-    return this.rate * this.days;
+    this.getSalary = function() {
+        return this.rate * this.days;
+    }
 }
 
 const worker1 = new Worker1('Иван', 'Иванов', 10, 31);
+
 console.log(worker1.name);
 console.log(worker1.surname);
 console.log(worker1.rate);
 console.log(worker1.days);
-
-worker1.getSalary = getSalary;
-
 console.log(worker1.getSalary());
-
 
 // ES 6:
 class Worker2 {
