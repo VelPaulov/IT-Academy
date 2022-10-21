81ed0f96d860b1f890133cf7427bfb5280fb4cbb
VelPaulov <velyakishkin@gmail.com>
2022-10-21 16:25:48 +0300

    minor fixes

--- classwork/korzina/Card.js
+++ classwork/korzina/Card.js
@@ -1,7 +1,7 @@
 export default class Card extends HTMLElement {
     constructor() {
-        super();
-
+      super();
+      this.data = JSON.parse(this.getAttribute('data'));
     }
 
     connectCallback() {
@@ -11,14 +11,14 @@ export default class Card extends HTMLElement {
     render() {
         this.innerHTML = `
         <div class="card" style="width: 18rem;">
-        <img src="${this.data.preview}" class="card-img-top" alt="${this.data.title}">
-        <div class="card-body">
-          <h5 class="card-title">${this.data.title}</h5>
-          <p class="card-text">${this.data.description}</p>
-          <p><strong>${this.data.price}$</strong></p>
-          <button href="#" class="btn btn-primary">Add to cart</button>
+          <img src="${this.data.preview}" class="card-img-top" alt="${this.data.title}">
+          <div class="card-body">
+            <h5 class="card-title">${this.data.title}</h5>
+            <p class="card-text">${this.data.description}</p>
+            <p><strong>${this.data.price}$</strong></p>
+            <button href="#" class="btn btn-primary">Add to cart</button>
+          </div>
         </div>
-      </div>
         `;
     }
 }
